03c80960e2942eae80b30ac83980a15f6fe25fd7
Predictscores <bursaca@yahoo.com>
2025-08-03 18:21:25 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ import { DataProvider } from '../contexts/DataContext';
 export default function App({ Component, pageProps }) {
   return (
     <DataProvider>
-      <div className="max-w-6xl mx-auto px-4 md:px-8">
+      <div className="w-full px-6 md:px-12">
         <Component {...pageProps} />
       </div>
     </DataProvider>
