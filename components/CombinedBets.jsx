5d5f157b6c86cc6253b871dc3edc9f5de4a594de
Predictscores <bursaca@yahoo.com>
2025-08-10 10:23:33 +0200

    Update CombinedBets.jsx

--- components/CombinedBets.jsx
+++ components/CombinedBets.jsx
@@ -1,36 +1,29 @@
-// FILE: components/CombinedBets.jsx
-import React from 'react';
-import Tabs from './Tabs';
-import FootballBets from './FootballBets';
-import CryptoTopSignals from './CryptoTopSignals';
+import React from "react";
+import Tabs from "./Tabs";
+import FootballBets from "./FootballBets";
+import CryptoTopSignals from "./CryptoTopSignals";
 
-export default function CombinedBets() {
-  return (
-    <Tabs>
-      {/* COMBINED: 1/3 football (top 3) + 2/3 crypto (top 3) */}
-      <div label="Combined">
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
-          {/* Leva kolona (1/3): tri fudbalske kartice, jedna ispod druge */}
-          <div className="lg:col-span-1 space-y-4">
-            <FootballBets limit={3} />
-          </div>
-
-          {/* Desna kolona (2/3): tri kripto kartice u gridu */}
-          <div className="lg:col-span-2">
-            <CryptoTopSignals limit={3} />
-          </div>
-        </div>
+const CombinedBets = () => (
+  <Tabs>
+    <div label="Combined">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+        {/* Levo: top 3 football */}
+        <FootballBets limit={3} />
+        {/* Desno: top 3 crypto */}
+        <CryptoTopSignals limit={3} />
       </div>
+    </div>
 
-      {/* FOOTBALL tab: prikaži sve (ili top 10, po tvom izboru) */}
-      <div label="Football">
-        <FootballBets limit={10} />
-      </div>
+    <div label="Football">
+      {/* Top 10 football (ili koliko god vrati – bez ograničenja ovde) */}
+      <FootballBets />
+    </div>
 
-      {/* CRYPTO tab: po želji top 10 */}
-      <div label="Crypto">
-        <CryptoTopSignals limit={10} />
-      </div>
-    </Tabs>
-  );
-}
+    <div label="Crypto">
+      {/* Top 10 crypto */}
+      <CryptoTopSignals limit={10} />
+    </div>
+  </Tabs>
+);
+
+export default CombinedBets;
