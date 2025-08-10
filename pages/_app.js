7233c5de5b8827828047b26871c0f03307b4c11a
Predictscores <bursaca@yahoo.com>
2025-08-10 13:10:08 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,8 @@
-// pages/_app.js
-import "@/styles/globals.css";
-import { DataProvider } from "../contexts/DataContext";
+// FILE: pages/_app.js
+import "../styles/globals.css";
+import DataProvider from "../contexts/DataContext";
 
-export default function App({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }) {
   return (
     <DataProvider>
       <Component {...pageProps} />
