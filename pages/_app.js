a273ffa4ff25e92a9a4b76c6f5064489c007732a
Predictscores <bursaca@yahoo.com>
2025-08-11 15:33:39 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 // FILE: pages/_app.js
 import "../styles/globals.css";
 import DataProvider from "../contexts/DataContext";
+import Head from "next/head";
+import { useEffect } from "react";
 
 export default function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    if (typeof window !== "undefined" && "serviceWorker" in navigator) {
+      navigator.serviceWorker.register("/sw.js").catch(() => {});
+    }
+  }, []);
+
   return (
     <DataProvider>
+      <Head>
+        <meta name="theme-color" content="#0f1116" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icons/icon-192.png" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+      </Head>
       <Component {...pageProps} />
     </DataProvider>
   );
