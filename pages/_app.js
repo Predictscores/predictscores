3e1cee809df4f91ee2d38d6ed0cab0254c6a4744
Predictscores <bursaca@yahoo.com>
2025-08-28 19:07:41 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 // pages/_app.js
 import "../styles/globals.css";
+import { DataProvider } from "../contexts/DataContext";
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <DataProvider><Component {...pageProps} /></DataProvider>;
 }
